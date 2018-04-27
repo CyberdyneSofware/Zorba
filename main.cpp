13ec9e46cba5455934c0dc186cb304ad7b6af430
Makenzie Trimble <34491985+Makamess@users.noreply.github.com>
2018-04-27 15:00:46 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "InventoryCommand.h"
+#include "InventoryClass.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //this is a temporary file which allows you to test the inventory code while it's being written
+    inventoryClass run;
+    run.choice = ;
+    run.inventoryCommand(choice)
+
     return 0;
 }
