dd04658566207c6129a68b62535d41b0ca0bcb79
Makenzie Trimble <34491985+Makamess@users.noreply.github.com>
2018-04-27 14:59:48 -0700

    Add files via upload

--- InventoryClass.h
+++ InventoryClass.h
@@ -25,9 +25,11 @@ private:
     weapon = 'w' - modifies strength
     clothing = 'c' - modifies defence
     buffs: HP = 'h' - modifies HP
-    buffs: intelligence = 'i' - modifies intelligence 
+    buffs: intelligence = 'i' - modifies intelligence
     buffs: Resistance = 'r' - modifies resistance
     ****/
+
+    char choice; //when calling the inventorycommand(choice); function please set choice to either 'a' for adding an item or 'b' for anything else
 
     string name; //stores the name
     char type; //stores the type of the item
@@ -127,7 +129,7 @@ private:
         inuse = false;
 
         //message
-        message = "The sword has to be almost 8 feet long and the handle has a bandage wrapped around it, you wonder how anyone can pick this thing up. With a lot of effort you are able to lift it off the ground. On the bandage there is something written in scrambled letters: W arni ng  y ou  must hav e  a tragi c back  s tory to use thi s  weapon.";
+        message = "The sword has to be almost 8 feet long and the handle has a bandage wrapped around it, \nyou wonder how anyone can pick this thing up.\n With a lot of effort you are able to lift it off the ground. \nOn the bandage there is something written in scrambled letters: W arni ng  y ou  must hav e  a tragi c back  s tory to use thi s  weapon.";
 
     };
 
@@ -165,7 +167,7 @@ private:
         inuse = false;
 
         //message
-        message = "The weapon is a weirdly shaped boot, you wonder if you can even put it on. attached to the front is what looks like the front of a gun. Written mysteriously on the side with a sharpie is the words: Kick to Shoot.";
+        message = "The weapon is a weirdly shaped boot, you wonder if you can even put it on. \nattached to the front is what looks like the front of a gun. \nWritten mysteriously on the side with a sharpie is the words: Kick to Shoot.";
     };
 
     //last weapon in the game helps you defeat the boss, op af, get it at the end of the game
@@ -223,7 +225,7 @@ private:
         inuse = false;
 
         //message
-        message = "It looks a bit worn, but kind of neat. Some of the string sewing it together has unraveled and is barely holding a pin to the front. (Are you cosplaying as Phillip?)"
+        message = "It looks a bit worn, but kind of neat. Some of the string sewing it together has unraveled and is barely holding a pin to the front. \n(Are you cosplaying as Phillip?)"
     };
 
     struct blackandwhitejacket
@@ -241,7 +243,7 @@ private:
         inuse = false;
 
         //message
-        message = "It is a thin jacket with a zipper in the front, you don't know who left this behind. (You can almost be as edgy as Alexi with this)";
+        message = "It is a thin jacket with a zipper in the front, you don't know who left this behind. \n(You can almost be as edgy as Alexi with this)";
 
     };
 
@@ -262,184 +264,239 @@ private:
         //message
         message = "It's a tin bottle with neon blue paint across the front, on the top there is a black cap keeping it closed "
     };
-    
+
     struct jeanjacket
     {
-        //name 
+        //name
         name = "Jean Jacket";
-        
-        //type 
+
+        //type
         type = 'c';
-        
+
         //modify stat
         modifyStat = 2;
-        
-        //in use 
-        inuse = false; 
-        
+
+        //in use
+        inuse = false;
+
         //message
         message = "The Jacket is a rough material, and is a sharp dark blue color. Maybe wearing it will make you look pretty cool.";
-        
+
     }:
 
-    struct leatherarmor 
+    struct leatherarmor
     {
-        //name 
+        //name
         name = "Leather Armor";
-            
-        //type 
+
+        //type
         type = 'c';
-        
+
         //modify stat
         modifyStat = 3;
-        
-        //in use 
+
+        //in use
         inuse = false;
-        
+
         //message
-        message = "The armor is made of what looks like leather, but when you put it under the light you can see that it is just a faux leather. Upon closer inspection it's appears to just be a t-shirt that looks leather.";
-            
+        message = "The armor is made of what looks like leather, \nbut when you put it under the light you can see that it is just a fuax leather. \nUpon closer inspection it's appears to just be a t-shirt that looks leather.";
+
     };
-    
+
     struct steelarmor
     {
-        //name 
+        //name
         name = "Steel Armor";
-        
-        //type 
+
+        //type
         type = 'c';
-        
+
         //modify stat
         modifyStat = 5;
-        
-        //in use 
-        inuse = false; 
-        
+
+        //in use
+        inuse = false;
+
         //message
         message = "The armor is relatively heavy, and cold at the touch. Surely it'll protect you in battle";
-        
+
     };
-    
-    //last clothing in the game 
+
+    //last clothing in the game
     struct diamondarmor
     {
-        //name 
+        //name
         name = "Diamond Armor";
-        
-        //type 
+
+        //type
         type = 'c';
-        
+
         //modify stat
         modifyStat = 100;
-        
-        //in use 
-        inuse = false; 
-        
+
+        //in use
+        inuse = false;
+
         //message
-        message = "This armor shimmers brighter than even the most bright highlighter. This will be the last armor you get in this game use it wisely.";
-        
+        message = "This armor shimmers brighter than even the most bright highlighter. \nThis will be the last armor you get in this game use it wisely.";
+
     };
-        
+
     //buffs:HP
     struct hppotion
     {
-        //name 
+        //name
         name = "HP Potion";
-        
-        //type 
-        type = 'h';
-        
-        //modify stat
-        modifyStat = 15;
-        
-        //in use 
-        inuse = false;
-        
-        //message
-        message = "The potion is in a small glass bottle, which has some soupy red liquid at the bottom of the bottle.";
-        
-    };
-    
-    struct safetycake
-    {
-        //name 
-        name = "Safety Cake";
-        
-        //type 
-        type = 'h';
-        
-        //modify stat
-        modifyStat = 25;
-        
-        //in use 
-        inuse = false; 
-        
-        //message
-        message = "It's a obnoxiously large cake with brightly colored frosting. It looks a bit odd in this environment and you wonder how it got here. Of course, it's a piece of cake to bake a pretty cake so someone probably had fun making it.";  
-        
-    };
-    
-    struct superpotion
-    {
-        //name 
-        name = "Super Potion";
-        
+
         //type
         type = 'h';
-        
+
+        //modify stat
+        modifyStat = 15;
+
+        //in use
+        inuse = false;
+
+        //message
+        message = "The potion is in a small glass bottle, which has some soupy red liquid at the bottom of the bottle.";
+
+    };
+
+    struct safetycake
+    {
+        //name
+        name = "Safety Cake";
+
+        //type
+        type = 'h';
+
+        //modify stat
+        modifyStat = 25;
+
+        //in use
+        inuse = false;
+
+        //message
+        message = "It's a obnoxiously large cake with brightly colored frosting. \nIt looks a bit odd in this environment and you wonder how it got here. \nOf course, it's a piece of cake to bake a pretty cake so someone probably had fun making it.";
+
+    };
+
+    struct superpotion
+    {
+        //name
+        name = "Super Potion";
+
+        //type
+        type = 'h';
+
         //modify stat
         modifyStat = 27;
-        
-        //in use 
+
+        //in use
         inuse = false;
-        
+
         //message
-        message = "It's a glass bottle with a liquid in it. When you pick it up you notice that it is glowing brightly, and the space around it is tinted slightly red.";
-        
+        message = "It's a glass bottle with a liquid in it. \nWhen you pick it up you notice that it is glowing brightly and the space around it is tinted slightly red.";
+
     };
-    
+
     //buffs: intelligence
     struct magicleaf
     {
-        //name 
+        //name
         name = "Magic Leaf";
-        
+
         //type
         type = 'i';
-        
+
         //modify stat
         modifyStat = 1;
-        
+
         //in use
         inuse = false;
-        
+
         //message
-        message = "It seems like just a leaf, but upon closer inspection, it seems to be magical. Eating it may increase your intelligence somehow.";
-        
+        message = "It seems like just a leaf, \nbut upon closer inspection, \nit seems to be magical. Eating it may increase your intelligence somehow.";
+
     };
-    
+
     struct largebook
     {
-        //name 
+        //name
         name = "Large Book";
-        
+
         //type
         type = 'i';
-        
+
         //modify stat
         modifyStat = 4;
-        
+
         //in use
         inuse = false;
-        
+
         //message
-        message = "The book is dusty so you can't see the cover, but when you pick it up some of it brushes off and you can see the words on the front. It says HERE LIES YOUR EDUMACATION";
-        
+        message = "The book is dusty so you can't see the cover, \nbut when you pick it up some of it brushes off and you can see the words on the front. \nIt says HERE LIES YOUR EDUMACATION. It will  ";
+
     };
-    
-    //buffs: Resistance 
-    
-    
+
+    struct thinkingcap
+    {
+        //name
+        name = "Thinking Cap";
+
+        //type
+        type = 'i';
+
+        //modify stat
+        modifyStat = 6;
+
+        //in use
+        inuse = false;
+
+        //message
+        message = "Its a hat made out of a silky fabric with paper stars taped on, very smart looking.";
+
+    };
+
+    //buffs: Resistance
+    struct woodenshield
+    {
+        //name
+        name = "Wooden Shield";
+
+        //type
+        type = 'r';
+
+        //modify stat
+        modifyStat = 1;
+
+        //in use
+        inuse = false;
+
+        //message
+        message = "The shield looks like it had just been carved out of a tree, not very sturdy, but it'll work.";
+
+    };
+
+    struct antiintelligencepotion
+    {
+        //name
+        name = "Anti Intelligence Potion";
+
+        //type
+        type = 'r';
+
+        //modify stat
+        modifyStat = 5;
+
+        //in use
+        inuse = false;
+
+        //message
+        message = "It's a potion which makes intelligence attacks less effective and increases your resistance.";
+
+    };
+
 
 
 };
